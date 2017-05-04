4fbd5a91971fa065689091d99e95d01b0c4d82cf
Molly <molly@mollygearsolid.com>
2017-05-04 21:16:23 +0100

    split pools into multiple files and load automatically

--- tagpoolgen.py
+++ tagpoolgen.py
@@ -1,31 +1,31 @@
-import random, sys, inflect, re
+import random, sys, inflect, re, os
 
 inf = inflect.engine()
 
-def namify(string):
-    string=string.title()
-    string=string.replace(" The "," the ")
-    string=string.replace(" Of "," of ")
-    string=string.replace(" In "," in ")
-    string=string.replace(" From "," from ")
-    string=string.replace(" At "," at ")
-    string=string.replace(" And "," and ")
-    string=string.replace("-Chan","-chan")
-    string=string.replace("lll","ll-l")
-    string=string.replace("ttt","tt-t")
-    string=string.replace("fff","ff-f")
-    string=string.replace("rrr","rr-r")
-    string=string.replace("eee","ee-e")
-    string=string.replace("sss","ss-s")
-    string=string.replace("aa","a-a")
-    string=string.replace("'S ","'s ")
-    string=string.replace("s's ","s' ")
-    return string
+# def namify(string):
+#     string=string.title()
+#     string=string.replace(" The "," the ")
+#     string=string.replace(" Of "," of ")
+#     string=string.replace(" In "," in ")
+#     string=string.replace(" From "," from ")
+#     string=string.replace(" At "," at ")
+#     string=string.replace(" And "," and ")
+#     string=string.replace("-Chan","-chan")
+#     string=string.replace("lll","ll-l")
+#     string=string.replace("ttt","tt-t")
+#     string=string.replace("fff","ff-f")
+#     string=string.replace("rrr","rr-r")
+#     string=string.replace("eee","ee-e")
+#     string=string.replace("sss","ss-s")
+#     string=string.replace("aa","a-a")
+#     string=string.replace("'S ","'s ")
+#     string=string.replace("s's ","s' ")
+#     return string
 
 def handle_match(match):
     key = match.group(1)
-    if key in parts:
-        choice = random.choice(parts[key])
+    if key in pools:
+        choice = random.choice(pools[key])
         if match.group(3):
             op = match.group(3)
             if op == 'nia':
@@ -56,35 +56,38 @@ def generate(targetcount, startstring):
         results.append(result)
     return results
 
-def load_parts(fname):
-    partsfile=open(fname,'r')
-    global parts
-    parts={}
-    curkey=""
-    for line in partsfile:
-        line=line.rstrip("\n")
-        if line[0:1]=="$":
-            curkey = line[1:]
-            if not curkey in parts:
-                parts[curkey] = []
-        elif len(line) > 0:
-            parts[curkey].append(line)
-    partsfile.close()
+def load_pools():
+    global pools
+    pools={}
+
+    cwd = os.getcwd()
+    dirfiles = [f for f in os.listdir(cwd) if os.path.isfile(os.path.join(cwd, f))]
+
+    for fname in dirfiles:
+        fbasename, fext = os.path.splitext(fname)
+        if fext == '.tagpools':
+            poolsfile=open(fname,'r')
+            curkey=""
+            for line in poolsfile:
+                line=line.rstrip("\n")
+                if line[0:1]=="$":
+                    curkey = line[1:]
+                    if not curkey in pools:
+                        pools[curkey] = []
+                elif len(line) > 0:
+                    pools[curkey].append(line)
+            poolsfile.close()
 
 def main(argv):
-    partsfname = "pieces_game.txt"
-    if len(argv) > 0:
-        partsfname = argv[0]
-
     targetcount = 10
-    if len(argv) > 1:
-        targetcount = int(argv[1])
+    if len(argv) > 0:
+        targetcount = int(argv[0])
 
     startstring = "[GAME]"
-    if len(argv) > 2:
-        startstring = int(argv[2])
+    if len(argv) > 1:
+        startstring = int(argv[1])
 
-    load_parts(partsfname)
+    load_pools()
 
     results = generate(targetcount, startstring)
 
