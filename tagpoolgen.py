7cc137fb283bc3688d7f1981a1549a837bef3f28
Alex Lawson <cheswick13@hotmail.com>
2017-05-07 21:18:58 +0100

    trying another layer of complexity: flavors

--- tagpoolgen.py
+++ tagpoolgen.py
@@ -40,27 +40,51 @@ def handle_match(match):
         return choice
     return "KEY '" + key + "' MISSING"
 
-def replace_tags(string):
-    result = re.subn('\[([a-zA-Z0-9 ]*)\]({([([a-zA-Z0-9 ^]*)})?', handle_match, string)
-    if result[1] > 0:
-        return replace_tags(result[0])
-    else:
-        return result[0]
+def replace_tags(flavors, string):
+    def handler(match):
+        key = match.group(1)
+        choice = replace_tags(flavors, pick_from_pool(flavors, key))
+        if match.group(3):
+            op = match.group(3)
+            if op == 'nia':
+                choice = inf.a(choice)
+            if op == 'np':
+                choice = inf.plural_noun(choice)
+            elif op == 'vpp':
+                choice = inf.present_participle(choice)
+            elif op == 'vp':
+                choice = inf.plural_verb(choice)
+        return choice
+
+    result = re.subn('\[([a-zA-Z0-9 ]*)\]({([([a-zA-Z0-9 ^]*)})?', handler, string)
+    return result[0]
 
 def generate(targetcount, startstring):
     results = []
     while len(results) < targetcount:
-        result = replace_tags(startstring)
+        flavors = choose_flavors(2)
+        result = replace_tags(flavors, startstring)
         result = result[0].upper() + result[1:]
+        result = '{}    {}'.format(result, flavors)
         # result = namify(result)
         print result
         results.append(result)
     return results
 
+def choose_flavors(targetcount):
+    targetcount = min(targetcount, len(pools.keys()))
+    flavors = ["base"]
+    while len(flavors) < targetcount:
+        f = random.choice(pools.keys())
+        if not f in flavors:
+            flavors.append(f)
+    return flavors
+
 def load_pools():
     global pools
     pools={}
 
+    global poolsums
     poolsums = {}
 
     cwd = os.getcwd()
@@ -69,40 +93,67 @@ def load_pools():
     for fname in dirfiles:
         fbasename, fext = os.path.splitext(fname)
         if fext == '.tagpools':
+            fnameparts = fbasename.split('_')
+            flavor = fnameparts[0]
+            if not flavor in pools:
+                pools[flavor] = {}
+                poolsums[flavor] = {}
+
             poolsfile=open(fname,'r')
             curkey=""
             for line in poolsfile:
                 line=line.rstrip("\n")
                 if line[0:1]=="$":
                     curkey = line[1:]
-                    if not curkey in pools:
-                        pools[curkey] = []
-                        poolsums[curkey] = 0
+                    if not curkey in pools[flavor]:
+                        pools[flavor][curkey] = []
+                        poolsums[flavor][curkey] = 0
                 elif len(line) > 0:
                     lineparts = line.split(' ')
                     try:
                         itemweight = float(lineparts[0])
-                        pools[curkey].append([itemweight, str.join(' ', lineparts[1:])])
-                        poolsums[curkey] += itemweight
+                        pools[flavor][curkey].append([itemweight, str.join(' ', lineparts[1:])])
+                        poolsums[flavor][curkey] += itemweight
                     except ValueError:
-                        pools[curkey].append([1.0, line])
-                        poolsums[curkey] += 1
+                        pools[flavor][curkey].append([1.0, line])
+                        poolsums[flavor][curkey] += 1
             poolsfile.close()
 
-    for poolname, pool in pools.iteritems():
-        curweight = 1.0
-        poolsum = poolsums[poolname]
-        for i, item in enumerate(pool):
-            itemweight = item[0] / poolsum
-            curweight -= itemweight
-            pool[i][0] = curweight
+    for flavor, flavorpools in pools.iteritems():
+        for poolname, pool in flavorpools.iteritems():
+            if not poolsums[flavor][poolname]:
+                poolsums[flavor][poolname] = 0
 
-def pick_from_pool(poolname):
-    pool = pools[poolname]
-    randchoice = random.random()
-    for item in pool:
-        if randchoice >= item[0]:
-            return item[1]
+            # curweight = 1.0
+            # poolsum = poolsums[flavor][poolname]
+            # for i, item in enumerate(pool):
+                # poolsums[flavor][poolname] += item[0]
+                # itemweight = item[0] / poolsum
+                # curweight -= itemweight
+                # pool[i][0] = curweight
+
+def pick_from_pool(flavors, poolname):
+    # print("Trying to choose from pool {} with flavors {}".format(poolname, flavors))
+
+    totalsum = 0
+    for flavor in flavors:
+        if poolname in poolsums[flavor]:
+            totalsum += poolsums[flavor][poolname]
+
+    randchoice = random.random() * float(totalsum)
+
+    # print('random choice is {} total sum is {}'.format(randchoice, totalsum))
+
+    weightcounter = 0
+    for flavor in flavors:
+        for poolitem in pools[flavor][poolname]:
+            weightcounter += poolitem[0]
+            # print('weightcounter {} item {}'.format(weightcounter, poolitem))
+            if randchoice <= weightcounter:
+                # print('selected {}'.format(poolitem[1]))
+                return poolitem[1]
+
+    # print("FAILED to choose from pool {} with flavors {}".format(poolname, flavors))
 
 def main(argv):
     targetcount = 10
